fd1d192bb9248ea0056ab83dd6bf28741d7926d9
ZhanboYang710 <zyang710@bu.edu>
2024-12-07 23:23:13 -0500

    initializing restroom aviator project

---
+++ src/restroom.ts
@@ -0,0 +1,16 @@
+
+export interface restroom{
+    location: string;
+    building: string;
+    floor: number;
+    capacity: number;
+    type: string;
+    rating: number;
+    pro: string[];
+    con: string[];
+}
+
+export interface thumbnail{
+    
+
+}
