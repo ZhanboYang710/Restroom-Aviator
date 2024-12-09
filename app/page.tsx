627027029e05ac14ce9ccdd9a39d1cd79b756991
ZhanboYang710 <zyang710@bu.edu>
2024-12-09 12:01:49 -0500

    styling with styled-components

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 import React, { useState } from "react";
 import { restroom } from "../types";
+import styled from "styled-components";
 import BathroomFilter from "@/components/bathroom-filter";
 import BathroomList from "@/components/bathroom-list";
 
+const StyledDiv = styled.div`
+    margin: 0px 50px;
+`
 
 export default function Page() {
     const [filters, setFilters] = useState({
@@ -14,10 +18,10 @@ export default function Page() {
     });
 
     return (
-        <div> 
+        <StyledDiv> 
             <BathroomFilter filters={filters} setFilters={setFilters} />
             <BathroomList filters={filters} />
-        </div>
+        </StyledDiv>
     );
 }
 
--- components/ bathroom-card.tsx
+++ components/ bathroom-card.tsx
@@ -1,6 +1,20 @@
 import React from "react";
 import Link from "next/link";
 import { restroom } from "../types";
+import styled from "styled-components";
+
+const StyledConatiner = styled.div`
+    padding: 1rem;
+    background-color: Beige;
+`
+
+// style={{ cursor: "pointer", border: "1px solid #ddd", padding: "10px", margin: "10px 0" }}
+const StyledDiv = styled.div`
+    border-width = 2px;
+    color: black;
+`
+const StyledLink = styled(Link)`
+`
 
 type BathroomProps = {
     restroom: restroom;
@@ -8,16 +22,16 @@ type BathroomProps = {
 
 export default function BathroomCard( { restroom }: BathroomProps) {
     return (
-        <>
-        <Link href={`/restroom/${restroom.id}`}>
-            <div style={{ cursor: "pointer", border: "1px solid #ddd", padding: "10px", margin: "10px 0" }}>
+        <StyledConatiner>
+        <StyledLink href={`/restroom/${restroom.id}`}>
+            <StyledDiv>
                 <h2>{restroom.building}</h2>
                 <p>Floor: {restroom.floor}</p>
                 <p>Gender: {restroom.gender}</p>
                 <p>Accessible: {restroom.accessible ? "Yes" : "No"}</p>
                 <p>Overall Rating: {restroom.ratings?.overall || "N/A"}</p>
-            </div>
-        </Link>
-        </>
+            </StyledDiv>
+        </StyledLink>
+        </StyledConatiner>
     )
 }
