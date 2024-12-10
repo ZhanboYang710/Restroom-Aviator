bed8d00658f58a264b1a2908b36c1089b81a15e4
sychen10086 <123329572+sychen10086@users.noreply.github.com>
2024-12-09 19:08:55 -0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import { restroom } from "../types";
 import styled from "styled-components";
 import BathroomFilter from "@/components/bathroom-filter";
 import BathroomList from "@/components/bathroom-list";
+import AppHeader from "@/components/header";
+
 
 const StyledDiv = styled.div`
     margin: 0px 50px;
@@ -19,6 +21,7 @@ export default function Page() {
 
     return (
         <StyledDiv> 
+            <AppHeader/>
             <BathroomFilter filters={filters} setFilters={setFilters} />
             <BathroomList filters={filters} />
         </StyledDiv>
